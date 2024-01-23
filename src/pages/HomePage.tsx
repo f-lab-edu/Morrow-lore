dc763db8ff8d2f6322cf6d8df296a14a276daeeb
uvlee <128670179+uvlee@users.noreply.github.com>
2024-01-23 22:44:22 +0900

    Feat: 홈페이지 틀 퍼블리싱 완료

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import mainimg from "../assets/image/main.png";
-import main from "../assets/video/video.mp4";
+import { NavLink } from "react-router-dom";
+import best from "../assets/image/best.jpg";
 
 const StyleMain = styled.main`
   width: 100%;
@@ -12,25 +12,43 @@ const StyleSection = styled.section`
   width: 100%;
   height: 100vh;
   margin: 0 auto;
-  position: relative;
 
-  h2 {
-    position: absolute;
-    top: 35%;
-    left: 24%;
-    z-index: 2;
-    font-size: 5rem;
-    color: #3e3232;
-  }
-
-  figure {
+  .banner {
     width: 100%;
     height: 100%;
-    background-color: #010101;
+    background-color: #1f2a24;
+    position: relative;
 
-    video {
-      width: 100%;
-      height: 100%;
+    img {
+      display: block;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+
+    h2 {
+      position: absolute;
+      top: 16%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      z-index: 2;
+      color: #f5f5f7;
+      font-size: 3.8rem;
+    }
+
+    a {
+      position: absolute;
+      top: 78%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      z-index: 2;
+      color: #f5f5f7;
+      border-bottom: 1px solid transparent;
+
+      &:hover {
+        border-bottom: 1px solid #f5f5f7;
+      }
     }
   }
 `;
@@ -39,19 +57,10 @@ const HomePage: React.FC = () => {
   return (
     <StyleMain>
       <StyleSection>
-        <h2>MORROW LORE</h2>
-        <figure>
-          <video autoPlay muted loop poster={mainimg}>
-            <source src={main} type="video/mp4" />
-            <track
-              // src={mainvtt}
-              src="main.vtt"
-              kind="descriptions"
-              srcLang="kr"
-              label="Korean Descriptions"
-              default
-            />
-          </video>
+        <figure className="banner">
+          <img src={best} alt="배너이미지" />
+          <h2>MORROW LORE BEST COLLECTION</h2>
+          <NavLink to="/product">Learn more</NavLink>
         </figure>
       </StyleSection>
     </StyleMain>
