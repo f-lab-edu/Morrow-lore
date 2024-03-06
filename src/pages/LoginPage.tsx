c5158746f1ae5e7364b62e023141aeda185927fa
uvlee <128670179+uvlee@users.noreply.github.com>
2024-03-06 22:56:47 +0900

    Feat: 로그인 form 생성

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,23 +1,96 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyleMain = styled.main`
+const StyledLogin = styled.section`
   width: 100%;
-  height: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  h1 {
+    margin-bottom: 2rem;
+  }
 `;
 
-const StyleSection = styled.section`
-  width: 90%;
-  height: 85vh;
-  margin: 0 auto;
-  background-color: rgb(126, 99, 99, 0.3);
+const StyledLoginForm = styled.form`
+  width: 30%;
+
+  ul {
+    display: flex;
+    flex-direction: column;
+
+    li {
+      display: flex;
+      margin-bottom: 10px;
+    }
+  }
+`;
+
+const StyledLoginLabel = styled.label`
+  width: 80px;
+  font-weight: 500;
+`;
+
+const StyledLoginInput = styled.input`
+  &:required {
+    border: 2px solid green;
+  }
+
+  &:invalid {
+    border: 2px solid red;
+  }
+
+  &:valid {
+    border: 2px solid red;
+  }
+`;
+
+const StyledLoginButton = styled.button`
+  display: block;
+  width: 100px;
+  text-align: center;
+  font-weight: 600;
+  padding: 10px;
+  outline: none;
+  background: rgb(29, 29, 31, 0.1);
+  font-size: 1rem;
+  border: 1px solid rgb(29, 29, 31, 0.5);
+  margin: 1rem auto;
 `;
 
 const LoginPage: React.FC = () => {
   return (
-    <StyleMain>
-      <StyleSection />
-    </StyleMain>
+    <StyledLogin>
+      <h1>로그인</h1>
+      <StyledLoginForm action="login">
+        <ul>
+          <li>
+            <StyledLoginLabel htmlFor="inputId">아이디</StyledLoginLabel>
+            <StyledLoginInput
+              type="text"
+              name="inputId"
+              id="inputId"
+              required
+            />
+          </li>
+          <li>
+            <StyledLoginLabel htmlFor="inputPassword">
+              비밀번호
+            </StyledLoginLabel>
+            <StyledLoginInput
+              type="password"
+              name="inputPassword"
+              id="inputPassword"
+              required
+            />
+          </li>
+        </ul>
+        <StyledLoginButton type="submit">로그인</StyledLoginButton>
+      </StyledLoginForm>
+      <StyledLoginButton type="button">회원가입</StyledLoginButton>
+    </StyledLogin>
   );
 };
 
